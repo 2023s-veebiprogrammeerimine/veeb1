045a2def7a4fe7a9a9bbbcfaa5c45803a2813484
Andrus Rinde <andrusrinde@users.noreply.github.com>
2023-10-12 17:07:40 +0300

    Kuues tund lõppenud

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const timeInfo = require('./datetime_fnc');
+const fs = require("fs");
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res)=>{
 	//res.send('See töötab!');
@@ -9,9 +12,24 @@ app.get('/', (req, res)=>{
 	res.render('index');
 });
 
-app.get('/test', (req, res)=>{
-	res.send('Test läks suurepäraselt!');
-	//res.download('index.js');
+app.get('/timenow', (req, res)=>{
+	const dateNow = timeInfo.dateETformatted();
+	const timeNow = timeInfo.timeETformatted();
+	//res.render('timenow');
+	res.render('timenow', {nowD: dateNow, nowT: timeNow});
+});
+
+app.get('/wisdom', (req, res)=>{
+	let folkWisdom = [];
+	fs.readFile('public/txtfiles/vanasonad.txt', 'utf8', (err, data)=>{
+		if(err){
+			throw err;
+		}
+		else {
+			folkWisdom = data.split(';');
+			res.render('justlist', {h1: 'Vanasõnad', wisdom: folkWisdom});
+		}
+	});
 });
 
 app.listen(5100);
